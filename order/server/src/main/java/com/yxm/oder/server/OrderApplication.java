4183ad0b3d02d3cabb284074157ab08f2b35e85d
startshineye <13001955858@163.com>
2019-12-13 23:23:42 +0800

    服务容错:feign-hystrix的使用

--- order/server/src/main/java/com/yxm/oder/server/OrderApplication.java
+++ order/server/src/main/java/com/yxm/oder/server/OrderApplication.java
@@ -5,11 +5,14 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
 @EnableFeignClients(basePackages = "com.yxm.product.client")
 /*@SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker*/
 @SpringCloudApplication
+@ComponentScan(basePackages = "com.yxm")
 public class OrderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
--- order/server/src/main/java/com/yxm/oder/server/controller/HystrixController.java
+++ order/server/src/main/java/com/yxm/oder/server/controller/HystrixController.java
@@ -1,5 +1,4 @@
 package com.yxm.oder.server.controller;
-
 import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
@@ -7,19 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.Arrays;
-
 @RestController
 @DefaultProperties(defaultFallback = "defaultFallback")
 public class HystrixController {
-    @HystrixCommand(commandProperties = {
-          /*  @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value = "4000")*/
+  /*  @HystrixCommand(commandProperties = {
+          *//*  @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value = "4000")*//*
             @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),
             @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),
             @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "1000"),
             @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "30")
-    })
+    })*/
+    @HystrixCommand
     @GetMapping("/getProductInfoList")
     public String getProductInfoList(@RequestParam("number") Integer number){
         if(number % 2 == 0){
@@ -30,7 +28,6 @@ public class HystrixController {
          */
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.postForObject("http://127.0.0.1:8081/product/listForOrder", Arrays.asList("157875196366160022"),String.class);
-
       /*  throw new RuntimeException("发送异常了!");*/
     }
     /**
